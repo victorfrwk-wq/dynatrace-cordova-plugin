44cb1730607a63b672718a86b0af55c76fffa95d
Victor Tavares <victorhase@frwk.com.br>
2026-02-25 16:14:49 -0300

    Add initial dynatrace configuration file

---
+++ dynatrace.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    cordova : {
+        debug : false,
+        autoUpdate : true,
+        cspURL: "https://tha19564.live.dynatrace.com"
+    },
+
+    js : {
+        url : "https://tha19564.live.dynatrace.com?Api-Token=",
+    },
+
+    android : {
+        // Those configs are copied 1:1
+        config : `
+        dynatrace {
+            configurations {
+                defaultConfig {
+                    autoStart {
+                        applicationId '244915ad-72ea-4508-b1bb-2089e626a6f0'
+                        beaconUrl 'https://bf76574zti.bf.dynatrace.com/mbeacon'
+                    }
+                    hybridWebView {
+                        enabled true
+                    }
+                    agentBehavior.startupLoadBalancing true
+                    agentBehavior.startupWithGrailEnabled true
+                }
+            }
+        }
+        `
+    },
+
+    ios : {
+        // Those configs are copied 1:1
+        config : `
+        <key>DTXApplicationID</key>
+        <string>244915ad-72ea-4508-b1bb-2089e626a6f0</string>
+        <key>DTXBeaconURL</key>
+        <string>https://bf76574zti.bf.dynatrace.com/mbeacon</string>
+        <key>DTXHybridApplication</key>
+        <string>true</string>
+        <key>DTXStartupLoadBalancing</key>
+        <true/>
+        <key>DTXStartupWithGrailEnabled</key>
+        <true/>
+        `
+    }
+}
